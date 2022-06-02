7d4495d5e1ef5bb932628be6a058768f5720c968
Jaydeep Suthar <jaydeeptechnism@gmail.com>
2022-06-02 17:37:37 +0530

    Basic Backend Ready

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const app = require("express")();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server, {});
+
+io.on("connection", (socket) => {
+	console.log(`My Socket is `, socket);
+
+	socket.on("chat", (payload) => {
+		console.log(`payload is `, payload);
+		io.emit("chat", payload);
+	});
+});
+
+server.listen(8000, console.log(`server is active and running on http://localhost:8000/`));
